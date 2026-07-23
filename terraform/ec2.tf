f64c795d52a2649358327177f6b425945f89c02f
fernando <fernandotower@gmail.com>
2026-07-23 16:48:16 -0500

    update to gp2 storage

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "k3s_node" {
   EOF
 
   root_block_device {
-    volume_type           = "gp3"
+    volume_type           = "gp2"
     volume_size           = var.volume_size
     encrypted             = true
     delete_on_termination = true
